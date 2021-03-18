1ceacc605c1e430ea9efaa40d497270a0aa48632
usman23499 <usamn23499@gmail.com>
2021-03-19 02:17:27 +0500

    first

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <>
+    <h1>
+      Hello World In GatsbY
+    </h1>
+    
+    </>
+  )
+}
+
+export default IndexPage
